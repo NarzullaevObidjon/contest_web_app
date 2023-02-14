dac14a9339b3817e50f652ef1a906d516175cbcd
NarzullaevObidjon <obidsattarovich3600@gmail.com>
2023-02-14 17:16:56 +0500

    initial commit

--- src/main/java/uz/pdp/contest_web/servlets/HelloServlet.java
+++ src/main/java/uz/pdp/contest_web/servlets/HelloServlet.java
@@ -1,4 +1,4 @@
-package uz.pdp.contest_web;
+package uz.pdp.contest_web.servlets;
 
 import java.io.*;
 
@@ -7,20 +7,12 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
     public void init() {
-        message = "Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
